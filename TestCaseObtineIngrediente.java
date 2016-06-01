c5002a52a38d6e2233e29c737f3af1f41def6e0c
IordacheRobert <iordacherobertnicolae@gmail.com>
2016-06-01 15:54:56 +0300

    testCaseObtineIngrediente final

--- TestCaseObtineIngrediente.java
+++ TestCaseObtineIngrediente.java
@@ -2,11 +2,14 @@ package ro.ase.cts.teste;
 
 import static org.junit.Assert.*;
 
+import java.security.InvalidParameterException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import ro.ase.cts.exceptii.ExceptieNull;
+import ro.ase.cts.singletonAndProxy.Depozit;
 import ro.ase.cts.singletonAndProxy.ProxyDepozit;
 
 public class TestCaseObtineIngrediente {
@@ -29,7 +32,7 @@ public class TestCaseObtineIngrediente {
 
 		try {
 			depozit.obtineIngrediente(ingredient);
-			fail("Nu se poate obtine un produs null");
+			fail("Nu se poate obtine un ingredient null");
 		} catch (ExceptieNull ex) {
 
 		}
@@ -39,9 +42,28 @@ public class TestCaseObtineIngrediente {
 	@Test
 	public void testObtineIngredientExistent(){
 		String ingredient = "ketchup";
-
 		assertTrue("Verificare obtinere ingredient existent",depozit.obtineIngrediente(ingredient));
-		
 	}
 
+	
+	@Test
+	public void testObtineIngredientDimensiuneZero(){
+		String ingredient="";
+		try {
+			depozit.obtineIngrediente(ingredient);
+			fail("Nu se poate obtine un ingredient de dimensiune 0");
+		} catch (InvalidParameterException ex) {
+
+		}
+		
+	}
+	
+	
+	@Test
+	public void testObtineIngredientInexistent(){
+		String ingredient = "suc";
+		assertFalse("Verificare obtinere ingredient existent",depozit.obtineIngrediente(ingredient));
+	}	
+	
+	
 }
